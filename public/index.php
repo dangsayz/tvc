8204740e3ee7f2bfc46c61e5c18dd978bee03a54
dangzr1 <dangzr1@gmail.com>
2025-06-14 12:39:21 -0500

    commit

--- public/index.php
+++ public/index.php
@@ -1,2 +1,24 @@
 <?php
-echo 'TVC coming soon';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"] ?? '');
+    $email = htmlspecialchars($_POST["email"] ?? '');
+    $message = htmlspecialchars($_POST["message"] ?? '');
+
+    echo "<h2>Thank you, $name!</h2>";
+    echo "<p>Email: $email</p>";
+    echo "<p>Message: $message</p>";
+} else {
+?>
+    <h1>Contact Us</h1>
+    <form method="post">
+        <label>Name:</label><br/>
+        <input type="text" name="name"><br/><br/>
+        <label>Email:</label><br/>
+        <input type="email" name="email"><br/><br/>
+        <label>Message:</label><br/>
+        <textarea name="message"></textarea><br/><br/>
+        <button type="submit">Send</button>
+    </form>
+<?php
+}
+?>
